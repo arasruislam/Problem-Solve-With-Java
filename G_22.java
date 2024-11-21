ab6482999581d2bea2ff83c1b506506639953a2a
arasruislam <arasru01@gmail.com>
2024-11-21 15:58:39 +0600

    solve another problem

---
+++ G_22.java
@@ -0,0 +1,30 @@
+/* 
+ * Write a Java program to remove last occurrence of a character from string.
+ */
+
+import java.util.Scanner;
+
+public class G_22 {
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+
+      // variable
+      System.out.print("Enter a input: ");
+      String input = scan.nextLine();
+
+      System.out.print("Enter a character for find: ");
+      String ch = scan.nextLine();
+
+      int index = input.lastIndexOf(ch);
+
+      if (index != -1) {
+         String result = input.substring(0, index) + input.substring(index + 1);
+
+         System.out.println("Updated string: " + result);
+      } else {
+         System.out.println("The character '" + ch + "' is not found in the string.");
+      }
+
+      scan.close();
+   }
+}
